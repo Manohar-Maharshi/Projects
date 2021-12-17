07900cf6e504a2b4329e2987667980c5d7e4045e
Manohar-Maharshi <netwitch101@gmail.com>
2021-12-17 15:04:13 +0530

    [UPDT] - links to repos

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import { Icon } from '@iconify/react';
 
 const Card = () => {
 	return (
-		<div className="group hover:shadow-pink-600 shadow relative max-h-[17.5rem] cursor-pointer bg-gray-800 rounded-xl p-3 w-[17rem]">
+		<div className="group hover:shadow-pink-600 shadow relative max-h-[17.5rem] cursor-pointer bg-gray-800 rounded-xl p-3 w-[18rem] md:w-[17rem]">
 			<img className=" w-full max-h-[8rem] rounded-xl object-cover" src="https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80" alt="Hello" />
 			<div>
 				<small className="uppercase text-[0.6rem] font-medium text-gray-500">beginner</small>
@@ -18,12 +18,12 @@ const Card = () => {
 					</div>
 				</div>
 				<div className="absolute group-hover:flex hidden top-2 right-2 flex flex-col space-y-2 items-center justify-end">
-					<div title="Live Link : https://git.io/JDKqc" className="hover:bg-gray-200 cursor-pointer bg-gray-300 grid place-items-center rounded-full w-7 h-7">
+					<a href="https://git.io/JDKqc" target="_blank" title="Live Link : https://git.io/JDKqc" className="hover:bg-gray-200 cursor-pointer bg-gray-300 grid place-items-center rounded-full w-7 h-7">
 						<Icon icon="akar-icons:link-out" className="text-black h-3.5 w-3.5"/>
-					</div>
-					<div title="Github Repo : https://git.io/JDKqc" className="hover:bg-gray-200 cursor-pointer bg-gray-300 grid place-items-center rounded-full w-7 h-7">
+					</a>
+					<a href="https://git.io/JDKqc"  target="_blank" title="Github Repo : https://git.io/JDKqc" className="hover:bg-gray-200 cursor-pointer bg-gray-300 grid place-items-center rounded-full w-7 h-7">
 						<Icon icon="akar-icons:github-fill" className="text-black h-3.5 w-3.5"/>
-					</div>
+					</a>
 				</div>
 				<div className="py-1">
 					<p className="text-xs text-gray-400 line-clamp-3">
